2901cd2eea658d986510646d226b3e05946b55c2
Albert1707 <55683926+Albert1707@users.noreply.github.com>
2022-10-02 21:58:55 +0300

    Create number.c

---
+++ number.c
@@ -0,0 +1,14 @@
+ #include <stdio.h> 
+ #include <string.h>   
+ int main () 
+ { 
+  int a;   
+  char b[10];
+  printf("Введите целое число  ") ;  
+ scanf ("%d" , &a );
+  printf("%d", a);
+ printf("Введите строку ");
+ scanf("%s",&b);
+ printf("%s",b);
+ return 0 ;
+  }
